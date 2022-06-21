84f6e024b740324ff6d2c44424c09dfc5088cc26
andresmolinares <andresmolinaresb@gmail.com>
2022-06-21 17:45:33 -0500

    Added Icon and Lottie Splash :white_check_mark:

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/view/menu.dart';
+import 'package:todo_app/view/splash.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,6 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false, title: 'Material App', home: Menu());
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        home: Splash());
   }
 }

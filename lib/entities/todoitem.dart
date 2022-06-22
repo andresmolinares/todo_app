33e0cd8a49824dd9a64e802681f60d3aaed567f8
jsierrabravo <jsierra@lsv-tech.com>
2022-06-21 23:46:33 -0500

    Added update functionality

--- lib/entities/todoitem.dart
+++ lib/entities/todoitem.dart
@@ -1,12 +1,14 @@
 class ToDoItem {
   
+  final String id;
   final String description;
   final bool isActive;
 
-  ToDoItem({this.description = "", this.isActive = false});
+  ToDoItem({this.description = "", this.isActive = false, this.id = ""});
 
   factory ToDoItem.fromJson(Map<String, dynamic> json) {
     return ToDoItem(
+      id: json["id"],
       description: json["description"],
       isActive: json["isActive"]
     );
